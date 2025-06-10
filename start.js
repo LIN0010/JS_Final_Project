bcea03db4cb357b563610646f9a61032b58fc296
LIN0010 <104240565+LIN0010@users.noreply.github.com>
2025-06-11 05:21:14 +0800

    Update start.js

--- start.js
+++ start.js
@@ -1,9 +1,9 @@
 
 function startGame() {
-	alert("開始遊戲！但是還沒做出來呢，哈哈哈～");
+	window.location.href = "https://www.google.com/";
 }
 
 function goHome() {
     // 退縮（回主頁）
-    window.location.href = "https://www.google.com/";
+	window.location.href = "https://www.google.com/";
 }
