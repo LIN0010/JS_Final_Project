6fe01f6045afd62980e4af65c619328d71882552
LIN0010 <104240565+LIN0010@users.noreply.github.com>
2025-06-11 05:24:03 +0800

    Update start.js

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 
 function startGame() {
-	window.location.href = "https://www.google.com/";
+	window.location.href = "https://lin0010.github.io/JS_Final_Project/illustrate/index.html";
 }
 
 function goHome() {
